bd74445e2891a0cc07583cff31a1ab13b13c76d9
Muskan <muskannsiingh@gmail.com>
2025-07-14 20:40:24 +0530

    commit 7

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   },
 });
 
